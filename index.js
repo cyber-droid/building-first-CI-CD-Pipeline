8a4bec99493f9a21ba5728a8ca65f8af89cd4ebb
Karanam Bhargav Balaji Naidu <karnam340@gmail.com>
2026-06-09 09:15:23 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello Iam Bhargav');
+console.log('Iam Building my first CI/CD Pipeline using GitHub Actions');
---
+++ test.js
@@ -0,0 +1,4 @@
+console.log('start test....');
+
+setTimeout(() => console.log('Waiting 3 seconds....'), 3000);
+console.log('Tests complete!');
